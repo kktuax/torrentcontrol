ff9595c7ecb9386f024c6bac24f04629565e4938
Max Tuni <maxilandia@gmail.com>
2013-09-15 14:46:34 +0200

    aux functions

--- torrentdownloader.py
+++ torrentdownloader.py
@@ -1,5 +1,22 @@
-import re
+import re, json, os, urllib2
 from eztv import EztvMagnetProvider
+
+def submit_to_transmission(transmission_location, link):
+	url = transmission_location + '/rpc?method=torrent-add&filename=' + link
+	urllib2.urlopen(url)
+
+def parse_json_conf(fname):
+	abspath = os.path.abspath(__file__)
+	dname = os.path.dirname(abspath)
+	os.chdir(dname)
+	conf = {}
+	if os.path.isfile(fname):
+		conf = json.load(open(fname))
+	return(conf)
+	
+
+conf = parse_json_conf('torrentdownloader.conf')
+
 
 providers = list()
 providers.append(EztvMagnetProvider())
